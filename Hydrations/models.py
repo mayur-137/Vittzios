83c4c8352da1f440ecd83bc1bc4813fef4cf5ed8
mayur savaliya <mayursavaliya150@gmail.com>
2023-08-09 15:03:20 +0530

    add cart model and try to fix cart logo in header

--- Hydrations/models.py
+++ Hydrations/models.py
@@ -76,3 +76,16 @@ class VitaminCapsules(models.Model):
     quantity = models.IntegerField(default=0)
     picture = models.ImageField(upload_to="static/images/VitaminCapsules/")
     created_on = models.DateTimeField(auto_now_add=True)
+
+
+class CartModel(models.Model):
+    id = models.AutoField
+    name = models.CharField(max_length=55)
+    description = models.CharField(max_length=255)
+    price = models.IntegerField()
+    discount = models.PositiveIntegerField()
+    slug = models.SlugField(unique=True, max_length=255)
+    stock = models.BooleanField()
+    quantity = models.IntegerField(default=0)
+    picture = models.ImageField(upload_to="static/images/VitaminCapsules/")
+    created_on = models.DateTimeField(auto_now_add=True)
